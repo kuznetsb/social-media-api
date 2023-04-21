61f2a0796d8e880207f6e4212ba8f998c7374c47
kuznetsb <kuznetsbog@gmail.com>
2023-04-21 15:47:37 +0300

    Created logout view

--- user/views.py
+++ user/views.py
@@ -1,7 +1,8 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-from user.serializers import UserSerializer
+from user.serializers import UserSerializer, LogoutSerializer
 
 
 class UserCreateView(generics.CreateAPIView):
@@ -14,3 +15,15 @@ class UserManageView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+class LogoutView(generics.GenericAPIView):
+    serializer_class = LogoutSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
