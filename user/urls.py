d0496c6bb49fdaf321b6661fb44144f67d29a4fd
kuznetsb <kuznetsbog@gmail.com>
2023-04-21 15:47:46 +0300

    Added logout url

--- user/urls.py
+++ user/urls.py
@@ -5,13 +5,14 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from user.views import UserCreateView, UserManageView
+from user.views import UserCreateView, UserManageView, LogoutView
 
 urlpatterns = [
     path("register/", UserCreateView.as_view(), name="create_user"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("me/", UserManageView.as_view(), name="manage_user"),
+    path("logout/", LogoutView.as_view(), name="auth_logout"),
 ]
 
 app_name = "user"
