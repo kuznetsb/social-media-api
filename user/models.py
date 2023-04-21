13bfae03183e87f7464989a874dc8f97d238f82c
kuznetsb <kuznetsbog@gmail.com>
2023-04-21 13:27:49 +0300

    Added image field with custom image path and bio field to User model

--- user/models.py
+++ user/models.py
@@ -1,5 +1,9 @@
+import os
+import uuid
+
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+from django.utils.text import slugify
 from django.utils.translation import gettext as _
 
 
@@ -37,9 +41,19 @@ class UserManager(BaseUserManager):
         return self._create_user(email, password, **extra_fields)
 
 
+def user_image_file_path(instance, filename):
+    _, ext = os.path.splitext(filename)
+
+    filename = f"{slugify(instance.info)}-{uuid.uuid4()}.{ext}"
+
+    return os.path.join("uploads/users/", filename)
+
+
 class User(AbstractUser):
     username = None
     email = models.EmailField(_("email address"), unique=True)
+    image = models.ImageField(null=True, upload_to=user_image_file_path)
+    bio = models.TextField(blank=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
