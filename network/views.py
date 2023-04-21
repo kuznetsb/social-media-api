a4faca777b85dcb9f9c7e8487137b04da466148f
kuznetsb <kuznetsbog@gmail.com>
2023-04-21 20:08:19 +0300

    Made search by email, first name and last name

--- network/views.py
+++ network/views.py
@@ -11,6 +11,22 @@ class UserViewSet(
     queryset = get_user_model().objects.all()
     permission_classes = (IsAuthenticated,)
 
+    def get_queryset(self):
+        queryset = get_user_model().objects.all()
+
+        email = self.request.query_params.get("email")
+        first_name = self.request.query_params.get("first_name")
+        last_name = self.request.query_params.get("last_name")
+
+        if email:
+            queryset = queryset.filter(email__icontains=email)
+        if first_name:
+            queryset = queryset.filter(first_name__icontains=first_name)
+        if last_name:
+            queryset = queryset.filter(last_name__icontains=last_name)
+
+        return queryset
+
     def get_serializer_class(self):
         if self.action == "list":
             return UserListSerializer
