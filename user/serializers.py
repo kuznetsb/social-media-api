ca16473b80e5e359b89f45fbabdf8313c59250dc
kuznetsb <kuznetsbog@gmail.com>
2023-04-21 19:11:28 +0300

    Added first_name and last_name to my detail page

--- user/serializers.py
+++ user/serializers.py
@@ -30,7 +30,16 @@ class UserSerializer(serializers.ModelSerializer):
 class MyDetailSerializer(UserSerializer):
     class Meta:
         model = get_user_model()
-        fields = ("id", "email", "password", "is_staff", "image", "bio")
+        fields = (
+            "id",
+            "email",
+            "password",
+            "first_name",
+            "last_name",
+            "is_staff",
+            "image",
+            "bio",
+        )
         read_only_fields = ("is_staff",)
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
