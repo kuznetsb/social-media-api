2429004ae0e83d3211b0930beea47b86966ef2d0
kuznetsb <kuznetsbog@gmail.com>
2023-04-22 23:16:11 +0300

    Added models for like posts and comment it

--- network/models.py
+++ network/models.py
@@ -30,9 +30,23 @@ class Post(models.Model):
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts"
     )
     created_at = models.DateTimeField(auto_now_add=True)
+    liked_by = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="liked_posts", blank=True
+    )
 
     class Meta:
         unique_together = ("title", "user")
 
     def __str__(self):
         return f"{self.title} post by {self.user} at {self.created_at}"
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments"
+    )
+
+    class Meta:
+        unique_together = ["content", "post", "user"]
