675878817f0eb361142c173c9da4fd4a313533ed
kuznetsb <kuznetsbog@gmail.com>
2023-04-22 11:57:45 +0300

    DDT installed

--- social_media/settings.py
+++ social_media/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
 
 # Application definition
 
@@ -41,10 +45,12 @@ INSTALLED_APPS = [
     "user",
     "rest_framework_simplejwt.token_blacklist",
     "network",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
--- social_media/urls.py
+++ social_media/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/", include("user.urls", namespace="user")),
     path("api/network/", include("network.urls", namespace="network")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
