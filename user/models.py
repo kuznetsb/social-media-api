7665f37492a37f590e3f4db5ba00f62432ccd515
kuznetsb <kuznetsbog@gmail.com>
2023-04-23 14:37:48 +0300

    deleted annotation

--- user/models.py
+++ user/models.py
@@ -2,7 +2,7 @@ import os
 import uuid
 
 from django.conf import settings
-from django.contrib.auth.models import AbstractUser, BaseUserManager, User
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.utils.translation import gettext as _
 
@@ -75,7 +75,7 @@ class User(AbstractUser):
             user += ", Full name:" + self.get_full_name()
         return user
 
-    def toggle_follow(self, user: User) -> None:
+    def toggle_follow(self, user) -> None:
         """Switch following parameter for user"""
         if user in self.followed_by.all():
             self.followed_by.remove(user)
